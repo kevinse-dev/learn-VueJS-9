e021f545e705a3feb317bd8698962f2f22773007
KAVIN SEBASTIAN <kavin.sebastian25@gmail.com>
2022-04-08 01:35:57 +0700

    validating Emited and Component V-model

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,50 @@
 <template>
+  <!-- <div id="app">
+    <button @click="showPopup = true" >Open to Popup</button>
+    <popup-comp @close="closePopup" v-show="showPopup" />
+  </div> -->
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <InputComp @modelValue="name"/>
+    <input type="text" v-model="text">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+// import PopupComp from './Components/PopupComp.vue'
+import InputComp from './Components/InputComp.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // PopupComp
+    InputComp
+
+  },
+  // data(){
+  //   return{
+  //     showPopup: false
+  //   }
+  // },
+  // methods:{
+  //   closePopup(name){
+  //     this.showPopup = false
+  //     console.log('name', name)
+  //   }
+  // }
+  data(){
+    return{
+      name:'',
+      text:''
+    }
+  },
+  methods:{
+    input(name){
+      this.name = name
+    }
   }
+
+
 }
 </script>
 
